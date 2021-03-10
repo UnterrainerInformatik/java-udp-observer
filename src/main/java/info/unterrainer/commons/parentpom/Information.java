0adebfca5554665bbb133898d8c5449475142fd4
Psilo <psilo@unterrainer.info>
2021-03-10 09:02:38 +0100

    repair information mechanism and bump version

--- src/main/java/info/unterrainer/commons/parentpom/Information.java
+++
@@ -1,7 +0,0 @@
-package info.unterrainer.commons.parentpom;
-
-public class Information {
-	public static final String name = "Parent-POM";
-	public static final String buildTime = "2020-10-22T06:11:52Z";
-	public static final String pomVersion = "0.1.1";
-}
---
+++ src/main/java/info/unterrainer/commons/udpobserver/Information.java
@@ -0,0 +1,7 @@
+package info.unterrainer.commons.udpobserver;
+
+public class Information {
+	public static final String name = "UDP-Observer";
+	public static final String buildTime = "2021-03-10T08:01:51Z";
+	public static final String pomVersion = "0.1.1";
+}
